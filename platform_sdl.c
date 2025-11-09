d487b20aa03d5a8ff309f16594b7c2a8b894abbb
not-kaz <87723604+not-kaz@users.noreply.github.com>
2025-11-09 12:03:02 +0100

    refactor: rename 'passed_conf' to 'custom_conf'

--- platform_sdl.c
+++ platform_sdl.c
@@ -7,7 +7,7 @@
 static void start_sdl_platform(struct platform *platform, 
 		struct platform_desc *platform_desc)
 {
-	struct platform_sdl_config *passed_conf = (struct platform_sdl_config *)platform_desc->config_handle;
+	struct platform_sdl_config *custom_conf = (struct platform_sdl_config *)platform_desc->config_handle;
 	struct platform_sdl_config conf = {
 		.init_flags = SDL_INIT_VIDEO | SDL_INIT_EVENTS,
 		.exec_pre_start_hook = NULL,
@@ -17,12 +17,12 @@ static void start_sdl_platform(struct platform *platform,
 	assert(platform);
 	if (platform_desc && platform_desc->config_handle 
 			&& platform_desc->config_handle_size == sizeof(struct platform_sdl_config)) {
-		conf.init_flags = passed_conf->init_flags;
-		if (passed_conf->exec_pre_start_hook) {
-			conf.exec_pre_start_hook = passed_conf->exec_pre_start_hook;
+		conf.init_flags = custom_conf->init_flags;
+		if (custom_conf->exec_pre_start_hook) {
+			conf.exec_pre_start_hook = custom_conf->exec_pre_start_hook;
 		}
-		if (passed_conf->exec_post_start_hook) {
-			conf.exec_post_start_hook = passed_conf->exec_post_start_hook;
+		if (custom_conf->exec_post_start_hook) {
+			conf.exec_post_start_hook = custom_conf->exec_post_start_hook;
 		}
 	}
 	if (conf.exec_pre_start_hook) {
@@ -46,7 +46,7 @@ static void init_sdl_window(struct platform *platform,
 		struct platform_window_handle *window, 
 		struct platform_window_desc *window_desc)
 {
-	struct platform_sdl_window_config *passed_conf = (struct platform_sdl_window_config *)window_desc->config_handle;
+	struct platform_sdl_window_config *custom_conf = (struct platform_sdl_window_config *)window_desc->config_handle;
 	struct platform_sdl_window_config conf = {
 		.init_flags = SDL_WINDOW_RESIZABLE
 	};
@@ -55,7 +55,7 @@ static void init_sdl_window(struct platform *platform,
 	assert(window);
 	if (window_desc && window_desc->config_handle 
 			&& window_desc->config_handle_size == sizeof(struct platform_sdl_window_config)) {
-		conf.init_flags = passed_conf->init_flags;
+		conf.init_flags = custom_conf->init_flags;
 	}
 	window->native_handle = SDL_CreateWindow(window_desc->title, window_desc->width, 
 			window_desc->height, conf.init_flags);
